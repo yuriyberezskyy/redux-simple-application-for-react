8a79036a498f3f900a4d4d1111835e12c152c90b
yuriyberezskyy <bereza95@gmail.com>
2020-05-20 00:57:17 -0400

    added connect to Counter.js and use dispatch with different types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,27 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+
   return state;
 };
 
