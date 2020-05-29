0e8f701a3db695e2684da6119876e2f4252c9e4f
yuriyberezskyy <bereza95@gmail.com>
2020-05-28 20:44:47 -0400

    added actions.js and also installed reduxdev

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,4 @@
-import * as actionTypes from "../actions";
+import * as actionTypes from "./actions/actions";
 
 const initialState = {
   results: [],
@@ -7,6 +7,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
+      console.log("hey");
       return {
         ...state,
         results: state.results.concat({ id: new Date(), value: action.result }),
